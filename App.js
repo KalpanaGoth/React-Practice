c66564ca1719f58cb595400c3f3e947fbc9f5f62
Goth <kalpana.goth@accenture.com>
2024-01-05 08:29:30 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,5 @@
+
+const heading = React.createElement("div",{id: "heading"},"Hello World");
+console.log("heading",heading);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
